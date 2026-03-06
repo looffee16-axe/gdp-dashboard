278e154add9fbf1355a1ddfcc50bd4bc6c775f8c
looffee16-axe <looffee16@gmail.com>
2026-03-06 14:43:01 +0000

    Initial application

---
+++ main.py
@@ -0,0 +1,103 @@
+import streamlit as st
+import pandas as pd
+import math
+from pathlib import Path
+
+# Set the title and favicon that appear in the Browser's tab bar.
+st.set_page_config(
+    page_title='My Sales Dashboard',
+    page_icon=':bicyclist:', # This is an emoji shortcode. Could be a URL too.
+)
+
+st.title('Bike Sales Dashboard')
+st.divider()
+
+# -----------------------------------------------------------------------------
+# Declare some useful functions.
+
+@st.cache_data
+def get_data():
+    DATA_FILENAME = Path(__file__).parent/'data/Sales_2015.csv'
+    fct_df1 = pd.read_csv(DATA_FILENAME)
+    DATA_FILENAME = Path(__file__).parent/'data/Sales_2016.csv'
+    fct_df2 = pd.read_csv(DATA_FILENAME)
+    DATA_FILENAME = Path(__file__).parent/'data/Sales_2017.csv'
+    fct_df3 = pd.read_csv(DATA_FILENAME)
+    DATA_FILENAME = Path(__file__).parent/'data/Territories.csv'
+    dim_territory_df = pd.read_csv(DATA_FILENAME)
+
+    df_list = [fct_df1,fct_df2,fct_df3]
+
+    fct_df = pd.concat(df_list, ignore_index=True)
+
+    df = pd.merge(fct_df,dim_territory_df,left_on='TerritoryKey',right_on='SalesTerritoryKey',how='left')
+
+    df['Year'] = df['OrderDate'].str.split('/').str[2]
+
+    # Convert years from string to integers
+    df['Year'] = pd.to_numeric(df['Year'])
+
+    MIN_YEAR = df['Year'].min()
+    MAX_YEAR = df['Year'].max()
+
+    df = df.groupby(['Year','Country']).agg({'OrderQuantity':'sum'}).reset_index()
+
+    return df
+
+df = get_data()
+''
+''
+min_value = df['Year'].min()
+max_value = df['Year'].max()
+
+from_year, to_year = st.slider(
+    'Which years are you interested in?',
+    min_value=min_value,
+    max_value=max_value,
+    value=[min_value, max_value])
+
+filter = df['Country'].unique()
+
+if not len(filter):
+    st.warning("Select at least one")
+
+selected_filter = st.multiselect(
+    'Which country would you like to view?',
+    filter,
+    ['United States'])
+
+''
+''
+''
+
+# Filter the data
+filtered_df = df[
+    (df['Country'].isin(selected_filter))
+    & (df['Year'] <= to_year)
+    & (from_year <= df['Year'])
+]
+
+st.header('Sales', divider='gray')
+
+''
+
+st.line_chart(
+    filtered_df,
+    x='Year',
+    y='OrderQuantity',
+    color='Country',
+)
+
+''
+''
+
+
+first_year = df[df['Year'] == from_year]
+last_year = df[df['Year'] == to_year]
+
+#st.header(f'Sales in {to_year}', divider='gray')
+
+''
+
+#cols = st.columns(4)
+
